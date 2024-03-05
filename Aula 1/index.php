23a18182bd55bd5ed78c43574f1422ceab76ce15
MarcelFlrs <127535882+MarcelFlrs@users.noreply.github.com>
2024-03-05 09:20:18 -0300

    Aula 1

---
+++ Aula 1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML Remember</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+    <div>
+      <a href="index.html">Home</a>
+      <a href="contato.html">Contato</a>
+      <a href="tabela.html">Tabela</a>
+    </div>
+</header>
+    <h1>aula PHP - lembrando do html</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit distinctio, esse ab mollitia a rerum odit adipisci excepturi tempore optio! Mollitia vel adipisci distinctio, numquam error eveniet deserunt. Eius. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur sapiente reiciendis ducimus debitis, aperiam voluptatum qui obcaecati. Perferendis possimus, unde sunt reiciendis vel porro praesentium, aspernatur ducimus vero quas hic.Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit distinctio, esse ab mollitia a rerum odit adipisci excepturi tempore optio! Mollitia vel adipisci distinctio, numquam error eveniet deserunt. Eius. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur sapiente reiciendis ducimus debitis, aperiam voluptatum qui obcaecati. Perferendis possimus, unde sunt reiciendis vel porro praesentium, aspernatur ducimus vero quas hic.Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit distinctio, esse ab mollitia a rerum odit adipisci excepturi tempore optio! Mollitia vel adipisci distinctio, numquam error eveniet deserunt. Eius. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur sapiente reiciendis ducimus debitis, aperiam voluptatum qui obcaecati. Perferendis possimus, unde sunt reiciendis vel porro praesentium, aspernatur ducimus vero quas hic.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit distinctio, esse ab mollitia a rerum odit adipisci excepturi tempore optio! Mollitia vel adipisci distinctio, numquam error eveniet deserunt. Eius. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur sapiente reiciendis ducimus debitis, aperiam voluptatum qui obcaecati. Perferendis possimus, unde sunt reiciendis vel porro praesentium, aspernatur ducimus vero quas hic.Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit distinctio, esse ab mollitia a rerum odit adipisci excepturi tempore optio! Mollitia vel adipisci distinctio, numquam error eveniet deserunt. Eius. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur sapiente reiciendis ducimus debitis, aperiam voluptatum qui obcaecati. Perferendis possimus, unde sunt reiciendis vel porro praesentium, aspernatur ducimus vero quas hic.Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit distinctio, esse ab mollitia a rerum odit adipisci excepturi tempore optio! Mollitia vel adipisci distinctio, numquam error eveniet deserunt. Eius. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur sapiente reiciendis ducimus debitis, aperiam voluptatum qui obcaecati. Perferendis possimus, unde sunt reiciendis vel porro praesentium, aspernatur ducimus vero quas hicLorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit distinctio, esse ab mollitia a rerum odit adipisci excepturi tempore optio! Mollitia vel adipisci distinctio, numquam error eveniet deserunt. Eius. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur sapiente reiciendis ducimus debitis, aperiam voluptatum qui obcaecati. Perferendis possimus, unde sunt reiciendis vel porro praesentium, aspernatur ducimus vero quas hic.Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit distinctio, esse ab mollitia a rerum odit adipisci excepturi tempore optio! Mollitia vel adipisci distinctio, numquam error eveniet deserunt. Eius. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur sapiente reiciendis ducimus debitis, aperiam voluptatum qui obcaecati. Perferendis possimus, unde sunt reiciendis vel porro praesentium, aspernatur ducimus vero quas hic.Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit distinctio, esse ab mollitia a rerum odit assdipisci excepturi tempore optio! Mollitia vel adipisci distinctio, numquam error eveniet deserunt. Eius. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur sapiente reiciendis ducimus debitis, aperiam voluptatum qui obcaecati. Perferendis possimus, unde sunt reiciendis vel porro praesentium, aspernatur ducimus vero quas hic</p>
+<?php
+$nomeVariavel = 10;
+echo "ola mundo ". $nomeVariavel;
+
+?>
+</body>
+</html>
