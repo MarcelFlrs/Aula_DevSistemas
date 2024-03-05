bf1e1cc0141e8b49acfee5ef336918f914ed27e8
MarcelFlrs <127535882+MarcelFlrs@users.noreply.github.com>
2024-03-05 09:20:28 -0300

    Aula 2

---
+++ Aula 2/aula2.php
@@ -0,0 +1,23 @@
+<?php
+
+// Comentario PHP
+
+echo "<h1> Olá mundo! <h1>";
+
+$num = 3;
+$texto = "String legal";
+$float = 5.7;
+$bool = false;
+
+$nome = "Jose da Silva";
+$idade = 87;
+$faculdade = true;
+
+echo"<div>";
+echo "Nome: " . $nome . "<br>";
+echo "Idade: " . $idade . "<br>";
+$faculdade = "ADS";
+echo "Faculdade: " . $faculdade . "<br>";
+echo"</div>";
+
+?>
