18eddc165848d8ef15218be2976ade678f1cf568
MarcelFlrs <127535882+MarcelFlrs@users.noreply.github.com>
2024-04-02 11:04:09 -0300

    ex01

---
+++ Aula 5/Lista 05/ex01.php
@@ -0,0 +1,11 @@
+<?php
+function somar($num1, $num2, $num3)
+{
+    $soma = $num1 + $num2 + $num3;
+    echo "<br> O resultado da soma dos números ({$num1}, {$num2} e {$num3}) é: {$soma}";
+}
+
+somar(10, 20, 30);
+somar(30, 6, 62);
+somar(84, 50, 20);
+somar(85, 65, 52);
