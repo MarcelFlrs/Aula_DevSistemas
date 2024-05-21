7a0c34a80d4e54dc62bf3bb151dd063d13aed946
MarcelFlrs <127535882+MarcelFlrs@users.noreply.github.com>
2024-05-21 08:58:22 -0300

    banco aula 12

--- Aula 12/banco.php
+++ Aula 12/banco.php
@@ -1,28 +1,37 @@
-<pre><?php
+<pre>
+<?php 
 
-        $banco = new mysqli("localhost:3307", "root", "", "bancodobrasil");
-        /* echo var_dump($banco);
-        echo "<br><br>----------------------------------------<br> <br>";
+    $banco = new mysqli("localhost", "root", "", "seunomedabasefeliz");
+    
+    // echo var_dump($banco);
+    
+    /*echo print_r($banco);
 
-        $busca = $banco->query("SELECT * FROM usuarios");
-        echo print_r($busca);
+    echo "<br>----------------------------------<br>";
+    $busca = $banco->query("SELECT * FROM usuarios");
+    echo print_r($busca);
+    
+    echo "<br>----------------------------------<br>";
+    // $obj = $busca->fetch_object();
+    // echo print_r($obj);
+    
+    echo "<br>----------------------------------<br>";
+    
+    while($obj = $busca->fetch_object()){
+        echo print_r($obj);
+    }
+    
+    echo "<br>----------------------------------<br>";
+    $busca = $banco->query("SELECT cod, usuario, nome, senha 
+    FROM usuarios WHERE usuario='gdc1000'");
 
-        // $obj = $busca->fetch_all();
-        // echo print_r($obj);
+    $usu = $busca->fetch_object();
+    echo print_r($usu);
 
-        echo "<br><br>----------------------------------------<br> <br>";
-        while ($obj = $busca->fetch_object()) {
-            echo print_r($obj);
-        }
+    echo "<br> C: " . $usu->cod;
+    echo "<br> U: " . $usu->usuario;
+    echo "<br> N: " . $usu->nome;
+    echo "<br> S: " . $usu->senha;*/
 
-        echo "<br><br>----------------------------------------<br> <br>";
-        $busca = $banco->query("SELECT cod, usuario, nome, senha FROM usuarios WHERE usuario='gdc1000'");
-        $usu = $busca->fetch_object();
-        echo print_r($usu);
-
-        echo "<br> C: " . $usu->cod;
-        echo "<br> U: " . $usu->usuario;
-        echo "<br> N: " . $usu->nome;
-        echo "<br> S: " . $usu->senha;
-        */
-        ?>
+?>
+</pre>
